ae2a81407414195c4ce59381cd91fe8ac89c53de
kbeksss <kbeksss@outlook.com>
2020-03-13 15:28:15 +0600

    kjlkjl

--- frontend/src/containers/Album.js
+++ frontend/src/containers/Album.js
@@ -1,20 +1,30 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {fetchTracks} from "../store/actions/tracksReducer";
 import {Card, Container} from "reactstrap";
 import {connect} from "react-redux";
 import {NavLink} from "react-router-dom";
 
 const Album = (props) => {
+    const [albumName, setAlbumName] = useState('');
     useEffect(() => {
         props.fetchTracks(props.match.params.albumId);
-        console.log(props.artists);
-        // eslint-disable-next-line
+        setAlbumName(() => {
+            let album = props.albums.filter(album => {
+                return album._id === props.match.params.albumId;
+            });
+            if(album.length){
+                return album[0].name;
+            } else{
+                return 'undefined'
+            }
+        })
     }, []);
 
     return (
         <Container className='mt-5'>
             <NavLink to='/'>Home</NavLink>
             <h2>Artist: {props.artists.length ? props.artists[0].name : 'Unknown'}</h2>
+            <h2>Album: {albumName}</h2>
             <h3>Tracks</h3>
             {props.tracks.map(track => (
                 <Card className='p-4 my-4' key={track._id}>
@@ -30,6 +40,7 @@ const Album = (props) => {
 const mapStateToProps = state => ({
     tracks: state.tracks.tracks,
     artists: state.artists.artists,
+    albums: state.albums.albums,
 });
 const mapDispatchToProps = dispatch => ({
     fetchTracks: (albumId) => dispatch(fetchTracks(albumId))
